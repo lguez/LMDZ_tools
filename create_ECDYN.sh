b982cab736b90aff50b093cf9d15fee92d818e56
Lionel GUEZ <guez@lmd.ens.fr>
2022-12-14 05:56:45 +0100

    Update to jean-zay and spirit
    Update from ergon and ciclad to jean-zay and spirit. Note we do not
    need rcp nor wget any longer: everything is local.

--- create_ECDYN.sh
+++ create_ECDYN.sh
@@ -4,11 +4,10 @@
 
 # This script creates the file "ECDYN.nc" at the first day, 0h, of a
 # given year and month, from ERA interim data at IDRIS or on
-# Ciclad. You need to be in the UNIX group subipsl at IDRIS, or in the
-# UNIX group ecmwf on Ciclad, to access the data. "ECDYN.nc" also
-# contains ST and CDSW (which could be in "ECPHY.nc").CDSW is not
-# modified because we do not care about that variable (and we do not
-# know what to put into it).
+# Spirit. On Spirit, you need to be in the UNIX group ecmwf to access
+# the data. "ECDYN.nc" also contains ST and CDSW (which could be in
+# "ECPHY.nc"). CDSW is not modified because we do not care about that
+# variable (and we do not know what to put into it).
 
 # This script calls the accompanying Python script "lnsp_to_SP.py",
 # which should be in the same directory.
@@ -23,32 +22,28 @@ module purge
 
 if [[ $IDRIS == y ]]
 then
-    ERAI_dir=ergon:~rpsl376/ERAI/NETCDF/GLOBAL_075/4xdaily
-    module load python/3.3.2
-
-    export PYTHONPATH=$PYTHONPATH:/workgpfs/rech/lmd/rlmd542/lib/python3.3/site-packages
-    # (for the netCDF4 python module at IDRIS)
+    ERAI_dir=/gpfsstore/rech/psl/rpsl376/ergon/ERAI/NETCDF/GLOBAL_075/4xdaily
+    module load nco
 else
-    # Ciclad:
+    # Spirit:
     ERAI_dir=/bdd/ERAI/NETCDF/GLOBAL_075/4xdaily
-    module load python/3.4-anaconda3
 fi
 
 set -x
 
 for var in u v ta r
 do
-    rcp $ERAI_dir/AN_PL/$year/$var.$year$month.aphei.GLOBAL_075.nc \
+    cp $ERAI_dir/AN_PL/$year/$var.$year$month.aphei.GLOBAL_075.nc \
 	${var}_${year}_$month.nc
 done
 
 for var in geopt stl1
 do
-    rcp $ERAI_dir/AN_SF/$year/$var.$year$month.ashei.GLOBAL_075.nc \
+    cp $ERAI_dir/AN_SF/$year/$var.$year$month.ashei.GLOBAL_075.nc \
 	${var}_${year}_$month.nc
 done
 
-rcp $ERAI_dir/AN_ML/$year/lnsp.$year$month.amhei.GLOBAL_075.nc \
+cp $ERAI_dir/AN_ML/$year/lnsp.$year$month.amhei.GLOBAL_075.nc \
     lnsp_${year}_$month.nc
 
 for var in u v ta r geopt stl1 lnsp
@@ -65,15 +60,7 @@ ncks --exclude --variable=level --overwrite plouf.nc \
     lnsp_${year}_${month}_01_00.nc
 
 # Start from CDSW so that the time value is overwritten:
-
-if [[ $IDRIS == y ]]
-then
-    rcp ergon:~rpsl035/IGCM/INIT/ATM/LMDZ/ECDYN.nc.20020101 ECDYN_high_res.nc
-else
-    # Ciclad:
-    wget http://www.lmd.jussieu.fr/~lmdz/LMDZ_Init/ECDYN_high_res.nc
-fi
-
+cp $R_IN/ATM/ECDYN.nc.20020101 ECDYN_high_res.nc
 ncks --variable=CDSW --overwrite ECDYN_high_res.nc \
     ECDYN_${year}_${month}_01_00.nc
 
